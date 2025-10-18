f7de19fddffa4021a82d2cc563f68fbe39107bc7
Fanox10 <estefanofer10@gmail.com>
2025-10-17 22:43:09 -0500

    version inicial del proyecto fintech

---
+++ src/main/java/com/fintech/AppFintech.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fintech;
+
+/**
+ *
+ * @author User
+ */
+public class AppFintech {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
