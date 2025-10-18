0f3401a1390617759e06acdfd17719013ba865c2
Leonardito-123 <Leonardocamposramos04@gmail.com>
2025-10-18 18:42:46 -0500

    Update Usuario.java

--- src/main/java/com/fintech/model/Usuario.java
+++ src/main/java/com/fintech/model/Usuario.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.fintech.model;
 
-/**
- *
- * @author User
- */
 public class Usuario {
-    
+
+private int id;
+private String nombre;
+private String correo;
+private int dni;
+private String clave;
+private int edad;
 }
