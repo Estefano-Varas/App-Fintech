2930a51f2b9f90f3ec962f781de31654139b6920
Mathiasvn <mathiasvargas344@gmail.com>
2025-10-23 18:44:32 -0500

    implementacion de usuario service

--- src/main/java/com/fintech/service/UsuarioService.java
+++ src/main/java/com/fintech/service/UsuarioService.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.fintech.service;
 
-/**
- *
- * @author User
- */
+import com.fintech.model.Usuario;
+
+
 public class UsuarioService {
+
+     private Usuario usuario;
+
+    
+    public void crearUsuario(int id, String nombre, String correo, int dni, String clave, int edad) {
+        usuario = new Usuario(id, nombre, correo, dni, clave, edad);
+        System.out.println("Usuario creado exitosamente.");
+    }
+
+    
+    public void mostrarUsuario() {
+        if (usuario != null) {
+            System.out.println(usuario);
+        } else {
+            System.out.println("No hay usuario registrado.");
+        }
+    }
+
+   
+    public void actualizarDatos(String nuevoNombre, String nuevoCorreo, int nuevaEdad) {
+        if (usuario != null) {
+            usuario.setNombre(nuevoNombre);
+            usuario.setCorreo(nuevoCorreo);
+            usuario.setEdad(nuevaEdad);
+            System.out.println("Datos actualizados correctamente.");
+        } else {
+            System.out.println("Debe crear un usuario primero.");
+        }
+    }
+
+    
+    public Usuario getUsuario() {
+        return usuario;
+    }
     
 }
