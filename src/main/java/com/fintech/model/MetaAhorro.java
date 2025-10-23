eaa6fe47982c8a88d727d6d801c35304dfc8944c
Mathias <mathias.vargasn@usil.pe>
2025-10-23 18:09:09 -0500

    Implementación del módulo Usuario y ajustes en MetaAhorro

--- src/main/java/com/fintech/model/MetaAhorro.java
+++ src/main/java/com/fintech/model/MetaAhorro.java
@@ -1,5 +1,7 @@
 package com.fintech.model;
 
+import java.time.LocalDate;
+
 public class MetaAhorro {
 
 private int id;
--- src/main/java/com/fintech/model/Usuario.java
+++ src/main/java/com/fintech/model/Usuario.java
@@ -8,4 +8,69 @@ private String correo;
 private int dni;
 private String clave;
 private int edad;
+
+    public Usuario(int id, String nombre, String correo, int dni, String clave, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.dni = dni;
+        this.clave = clave;
+        this.edad = edad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "id=" + id + ", nombre=" + nombre + ", correo=" + correo + ", dni=" + dni + ", clave=" + clave + ", edad=" + edad + '}';
+    }
+
+
+
 }
